4e9f899d439834339c0155d13c75371e05099f68
Alexis Ivan Bonvicini <alexisbonvicini@gmail.com>
2022-06-17 03:28:56 -0300

    Dependencies - Improvments in app.js

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-require('dotenv/config');
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 // Import Routes
-const postsRoute = require('./routes/posts');
+import { TaskRouter } from './src/routes/taskRoute.js';
+import { DeployRouter } from './src/routes/deployRoute.js';
 
-app.use('/posts', postsRoute);
-// app.use('/users', usersRoute);
-
-// Middlewares
-
-// Routes
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
+const app = express();
 
 //Connect with the database
-mongoose.connect(
-    process.env.DB_CONNECTION,
-    {
-        useNewUrlParser: true,
-    },
-    () => {
-        console.log('Connected to the database');
-    },
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+    console.log('Connected to the database');
+});
+
+// Middlewares
+app.use(express.json());
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    }),
 );
+
+// Routes Middleware
+app.use('/api/task', TaskRouter());
+app.use('/api/deploy', DeployRouter());
 
 // Como escuchamos el puerto 3000
 app.listen(3001);
